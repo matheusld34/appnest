6d3a6e7ed8e1d8f65076a7da774afc131d7e34b8
Matheus <matheus_lopes151@hotmail.com>
2025-04-03 20:39:32 -0300

    tasks service

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,16 +1,40 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+
 @Controller('tasks')
 export class TasksController {
     constructor(private readonly taskService: TasksService) { }
+
     @Get()
-    getTasks() {
-        return "Listasndo Todas Tarefas"
+    findAllTasks() {
+        return this.taskService.findAll()
     }
 
     @Get(":id")
-    getTest() {
-        return this.taskService.findOneTask();
+    findOneTask(@Param('id') id: string) {
+        return this.taskService.findOne(id);
+    }
+
+    @Post()
+    createTask(@Body() body: any) {
+        console.log(body)
+
+        return this.taskService.create(body)
+    }
+
+    @Patch(":id")
+    updateTask(@Param("id") id: string, @Body() body: any) {
+        console.log("ID: ", id)
+        console.log("Body: ", body)
+
+        return "Atualizando tarefa..."
+    }
+
+    @Delete(":id")
+    deleteTask(@Param("id") id: string) {
+        console.log("ID ENVIADO: " + id)
+
+        return "Deltar a terefa com id " + id
     }
 
 }
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -3,7 +3,8 @@ import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 
 @Module({
+  imports: [],
   controllers: [TasksController],
   providers: [TasksService]
 })
-export class TasksModule {}
+export class TasksModule { }
