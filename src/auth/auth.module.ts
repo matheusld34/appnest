5b97ed029db2d14d79c46226c3a0d8cb4b91cf52
Matheus <matheus_lopes151@hotmail.com>
2025-04-18 16:26:10 -0300

    criando sistema de hash

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { HashingServiceProtocol } from './hash/hashing.service';
+import { BcryptService } from './hash/bcrypt.service';
+
+@Module({
+    providers: [
+        {
+            provide: HashingServiceProtocol,
+            useClass: BcryptService
+        }
+    ]
+})
+export class AuthModule { }
